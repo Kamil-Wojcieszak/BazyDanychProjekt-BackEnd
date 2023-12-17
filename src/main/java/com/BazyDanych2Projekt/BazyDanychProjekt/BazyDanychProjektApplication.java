adcf0c1265ab3fa56e653df09b9bf888eab120a7
Kamil Wojcieszak <264487@student.pwr.edu.pl>
2023-12-17 17:57:51 +0100

    Initial project

---
+++ src/main/java/com/BazyDanych2Projekt/BazyDanychProjekt/BazyDanychProjektApplication.java
@@ -0,0 +1,14 @@
+package com.BazyDanych2Projekt.BazyDanychProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazyDanychProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BazyDanychProjektApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/BazyDanych2Projekt/BazyDanychProjekt/BazyDanychProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BazyDanych2Projekt.BazyDanychProjekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BazyDanychProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
